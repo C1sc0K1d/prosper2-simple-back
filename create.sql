c3d7377e882ab47d817382a349cf1ecb6a67e18c
Gabriel Calil Abrão <gabrielcalil12@hotmail.com>
2023-01-28 20:23:29 -0300

    dserve-port

--- create.sql
+++ create.sql
@@ -8,6 +8,6 @@ INSERT INTO TB_INSTRUMENTS(name, image, price, quantity, type) VALUES ('Piano', 
 INSERT INTO TB_INSTRUMENTS(name, image, price, quantity, type) VALUES ('Flute', 'assets/imgs/flute.jpg', 32.10, NULL, 'wind');
 INSERT INTO TB_INSTRUMENTS(name, image, price, quantity, type) VALUES ('Harp', 'assets/imgs/harp.jpg', 45.00, NULL, 'acords');
 INSERT INTO TB_INSTRUMENTS(name, image, price, quantity, type) VALUES ('Drums', 'assets/imgs/drums.jpg', 213.13, NULL, 'percussion');
-INSERT INTO TB_USER(user_name, password, is_admin) VALUES ('admin', 'admin', 1);
-INSERT INTO TB_USER(user_name, password, is_admin) VALUES ('claudia', '1234', 0);
-INSERT INTO TB_USER(user_name, password, is_admin) VALUES ('michael', '4321', 0);
+INSERT INTO TB_USER(user_name, password, is_admin) VALUES ('admin', 'admin', true);
+INSERT INTO TB_USER(user_name, password, is_admin) VALUES ('claudia', '1234', false);
+INSERT INTO TB_USER(user_name, password, is_admin) VALUES ('michael', '4321', false);
